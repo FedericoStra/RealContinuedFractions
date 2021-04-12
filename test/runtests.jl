170cb1a424202693a362799099cd4a857f089ee2
Federico Stra <stra.federico@gmail.com>
2021-04-12 19:31:46 +0200

    Add tests for `convergents`/`convergent`

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,8 +32,9 @@ using TestSetExtensions
         @test_throws OverflowError fromcontfrac(Int32, contfrac(big(π), 25))
         @test fromcontfrac(contfrac(big(π), 25)) == 8958937768937//2851718461558
         @test fromcontfrac(Int, contfrac(big(π), 25)) == 8958937768937//2851718461558
+        @test_throws OverflowError fromcontfrac(Int, contfrac(big(π), 35))
+        @test_throws OverflowError fromcontfrac(Int128, contfrac(big(π), 80))
         @test fromcontfrac(Float64, contfrac(big(π), 25)) == 3.141592653589793
-        @test_throws OverflowError fromcontfrac(Int32, contfrac(big(π), 75))
     end
     @testset "roundtrip" begin
         @testset "contfrac ∘ fromcontfrac" begin
@@ -53,5 +54,19 @@ using TestSetExtensions
                 @test fromcontfrac(contfrac(r)) == r
             end
         end
+    end
+    @testset "convergents" begin
+        @test convergents(contfrac(π, 12)) == [
+            3//1, 22//7, 333//106, 355//113, 103993//33102, 104348//33215, 208341//66317,
+            312689//99532, 833719//265381, 1146408//364913, 4272943//1360120, 5419351//1725033]
+        @test convergents(contfrac(big(π), 25))[end-5:end] == [
+            14885392687//4738167652, 21053343141//6701487259, 1783366216531//567663097408,
+            3587785776203//1142027682075, 5371151992734//1709690779483, 8958937768937//2851718461558]
+    end
+    @testset "convergent" begin
+        cf = contfrac(big(π), 35)
+        @test convergent(cf) == convergents(cf)[end]
+        @test convergent(cf) == -6844906428587781119//7849984890429250040
+        @test convergent(BigInt, cf) == 265099323460521503743//84383735478118508040
     end
 end
