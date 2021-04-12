21cc9e473f3bc8ad918f0dd7228b0d493945da91
Documenter.jl <documenter@juliadocs.github.io>
2021-04-12 13:46:33 +0000

    build based on 678f5d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RealContinuedFractions","category":"page"},{"location":"#RealContinuedFractions","page":"Home","title":"RealContinuedFractions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RealContinuedFractions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RealContinuedFractions]","category":"page"}]
+}
